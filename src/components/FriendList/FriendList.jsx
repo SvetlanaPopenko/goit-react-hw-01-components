342e9729cb9100dc0fb5a8b7e7c3f092feb1d3a7
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-10 20:48:51 +0200

    Create FriendList.jsx

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+    key: PropTypes.number.isRequired
+}
