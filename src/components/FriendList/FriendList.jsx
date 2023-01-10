d9491b72886959ce004727e09679e79919c30408
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-11 01:13:44 +0200

    Update FriendList.jsx

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -5,19 +5,16 @@ export const FriendList = ({ friends }) => {
   return (
     <ul class="friend-list">
       {friends.map(friend => {
-        return (
-          <FriendListItem
-            key={friend.id}
-            avatar={friend.avatar}
-            name={friend.name}
-            isOnline={friend.isOnline}
-          />
-        );
+        return <FriendListItem key={friend.id} friend={friend} />;
       })}
     </ul>
   );
 };
 
 FriendList.propTypes = {
-    key: PropTypes.number.isRequired
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.number.isRequired,
+    }).isRequired
+  ),
+};
