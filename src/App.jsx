88b80bdc6fbda59681803312b9e8c8d3dad54643
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-10 20:48:36 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+
+import user from './configs/user.json'; 
+import data from './configs/data.json';
+import friends from './configs/friends.json'
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />;
+    </div>
+  )
+};
