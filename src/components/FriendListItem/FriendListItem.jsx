beb770ad1c3dd9575f45f066032a31d286b70ebe
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-11 01:36:03 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 
-export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+export const FriendListItem = ({ friend: { id, avatar, name, isOnline } }) => {
   return (
-    <li class="item">
+    <li class="item" key={id}>
       <span class="status">{isOnline}</span>
       <img class="avatar" src={avatar} alt="User avatar" width="48" />
       <p class="name">{name}</p>
