f760736a511229ece6f75753449912f815b3bed8
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-15 17:00:01 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 
 export const FriendListItem = ({ friend: { id, avatar, name, isOnline } }) => {
   return (
-    <li class="item" key={id}>
-      <span class="status">{isOnline}</span>
-      <img class="avatar" src={avatar} alt="User avatar" width="48" />
-      <p class="name">{name}</p>
+    <li classclassName="item" key={id}>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
     </li>
   );
 };
 
 FriendListItem.propTypes = {
   friend: PropTypes.shape({
+    id:PropTypes.number.isRequired,
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
