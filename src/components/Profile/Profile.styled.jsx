1883e3d211fe7fb13f4d7bf0be537a19c322fa2d
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-16 00:40:20 +0200

    Create Profile.styled.jsx

---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+export const ProfileCard = styled.div`
+    display: flex;
+    align-items:center;
+    flex-direction:column;
+    gap: 8px;
+    padding: 50px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 400px;
+   border-radius:30px;
+`
+
+export const UserCard = styled.div`
+    display: flex;
+    align-items:center;
+    flex-direction:column;
+    justify-content: center;
+    gap: 15px;
+`
+export const UserAvatar = styled.img`
+    border-radius: 100%;
+`
+export const UserName= styled.p`
+    margin: 0 auto;
+`
+export const UserTag =styled.p`
+    margin: 0 auto;
+`
+ export const UserLocation=styled.p`
+    margin: 0 auto;
+ `
