dd36901a26121f45f3f691353e2e0c06ec8ced8c
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 01:15:19 +0200

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,49 @@
 import PropTypes from 'prop-types';
 import { GoLocation } from 'react-icons/go';
 import { HiOutlineUserCircle } from 'react-icons/hi';
-import { ProfileCard, UserCard,UserAvatar, UserName, UserTag,UserLocation } from './Profile.styled';
+import {
+  ProfileCard,
+  UserCard,
+  UserAvatar,
+  UserName,
+  UserTag,
+  UserLocation,
+  StatsList,
+  StatsItems,
+  StatsLabel,
+  StatsQuantity,
+} from './Profile.styled';
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <ProfileCard>
       <UserCard>
-        <UserAvatar src={avatar} alt={username} width="200px"/>
-        <UserName><HiOutlineUserCircle style={{width: "36", height: "36"}} />{username}</UserName>
+        <UserAvatar src={avatar} alt={username} width="200px" />
+        <UserName>
+          <HiOutlineUserCircle style={{ width: '36', height: '36' }} />
+          {username}
+        </UserName>
         <UserTag>@{tag}</UserTag>
-        <UserLocation><GoLocation style={{width: "36", height: "36"}} />{location}</UserLocation>
+        <UserLocation>
+          <GoLocation style={{ width: '36', height: '36' }} />
+          {location}
+        </UserLocation>
       </UserCard>
 
-      <ul className="stats">
-        <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
-        </li>
-        <li>
-          <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
-        </li>
-        <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
-        </li>
-      </ul>
+      <StatsList>
+        <StatsItems>
+          <StatsLabel>Followers</StatsLabel>
+          <StatsQuantity>{stats.followers}</StatsQuantity>
+        </StatsItems>
+        <StatsItems>
+          <StatsLabel>Views</StatsLabel>
+          <StatsQuantity>{stats.views}</StatsQuantity>
+        </StatsItems>
+        <StatsItems>
+          <StatsLabel>Likes</StatsLabel>
+          <StatsQuantity>{stats.likes}</StatsQuantity>
+        </StatsItems>
+      </StatsList>
     </ProfileCard>
   );
 };
