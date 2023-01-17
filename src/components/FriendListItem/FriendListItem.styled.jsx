18f37c7c61608114a8315d079df75e6234ed9327
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 02:24:38 +0200

    Create FriendListItem.styled.jsx

---
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const FriendCard = styled.li`
+    display: flex;
+    align-items: center;
+     justify-content: flex-start;
+  gap: 10px;
+  
+ 
+  padding: 6px;
+  background-color: #f7f5f5;
+  box-shadow: 1px 2px 2px 3px #616161;
+  width: 200px;
+`
