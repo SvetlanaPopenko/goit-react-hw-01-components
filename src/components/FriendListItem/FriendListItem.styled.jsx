df2e263636216d029396e49c99066b3bcb9aeef7
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 18:10:30 +0200

    Update FriendListItem.styled.jsx

--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -1,14 +1,38 @@
 import styled from '@emotion/styled';
 
 export const FriendCard = styled.li`
-    display: flex;
-    align-items: center;
-     justify-content: flex-start;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
   gap: 10px;
-  
- 
+
   padding: 6px;
-  background-color: #f7f5f5;
-  box-shadow: 1px 2px 2px 3px #616161;
-  width: 200px;
+  background-color: #26d2f3;
+  box-shadow: inset 0px 1px 45px 1px #0571d5;
+  width: 250px;
+`;
+
+export const FriendStatus = styled.span`
+  display: inline-block;
+  padding:5px;
+  border-radius: 50%;
+  width: 15px;
+  height: 15px;
+  background-color:white;
+  background-color: ${showStatus};
+`;
+
+function showStatus(props) {
+    return props.isOnline ? "green" : "red";
+}
+
+export const FriendAvatar=styled.img`
+    border-radius:25%;
+margin-right: 10px;
+    padding: 10px;
+    box-shadow: 0px 0px 10px 2px rgba(117, 3, 248, 0.81);
+`
+
+export const FriendName=styled.p`
+    color:#4d078e;
 `
