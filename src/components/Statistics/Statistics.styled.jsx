4060914e5349e7c9f73ae106faef43149de1fe3a
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 02:24:47 +0200

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,33 +1,38 @@
 import styled from '@emotion/styled';
 import { getRandomHexColor } from 'utils/RandomColor';
 
-export const StatisticsSection=styled.section`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    width: 600px;
-    border-radius:20px;
-    background-color:pink;
-`
-export const StatisticsTitle=styled.h2`
-    font-size:48px;
-background-color:pink;
-margin: 0;
-
-`
-export const StatisticsList=styled.ul`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-   
-`
+export const StatisticsSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 70%;
+  border-radius: 20px;
+  background-color: pink;
+  box-shadow: inset 5px 15px 27px -10px rgba(0, 0, 0, 0.5);
+`;
+export const StatisticsTitle = styled.h2`
+  font-size: 48px;
+  margin: 0;
+`;
+export const StatisticsList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 export const StatisticItem = styled.li`
-    display: flex;
+  display: flex;
   font-size: 20px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   padding: 10px 20px;
   background-color: ${getRandomHexColor};
+`;
+
+export const StatisticsLabel = styled.span`
+    color: white;
+`
+export const StatisticsPercentage = styled.span`
+    color: white;
 `
