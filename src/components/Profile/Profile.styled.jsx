4477f0d0e019993c4da09c2337b0d3883c8344e2
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 18:10:32 +0200

    Update Profile.styled.jsx

--- src/components/Profile/Profile.styled.jsx
+++ src/components/Profile/Profile.styled.jsx
@@ -1,67 +1,64 @@
 import styled from '@emotion/styled';
 
 export const ProfileCard = styled.div`
-    display: flex;
-    align-items:center;
-    flex-direction:column;
-    gap: 8px;
-    padding: 50px;
-    margin-left: auto;
-    margin-right: auto;
-    width: 400px;
-   border-radius:30px;
-   background-color: #8a2be266;
-    box-shadow: inset 5px 15px 27px -10px rgba(0, 0, 0, 0.5);
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 8px;
+  padding: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 400px;
+  border-radius: 30px;
+  background-color: #8a2be266;
+  box-shadow: inset 5px 15px 27px -10px rgba(0, 0, 0, 0.5);
+`;
 
 export const UserCard = styled.div`
-    display: flex;
-    align-items:center;
-    flex-direction:column;
-    justify-content: center;
-    gap: 15px;
-`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  gap: 15px;
+`;
 export const UserAvatar = styled.img`
-    border-radius: 50%;
-    background-color: #fff;
-`
-export const UserName= styled.p`
-    margin: 0 auto;
-    font-weight:bold;
-`
-export const UserTag =styled.p`
-    margin: 0 auto;
-`
- export const UserLocation=styled.p`
-    margin: 0 auto;
- `
- export const StatsList=styled.ul`
-    display: flex;
-    align-items:center;
-    justify-content:center;
-    width: 100%;
-    padding: 0;
-    `
+  border-radius: 50%;
+  background-color: #fff;
+`;
+export const UserName = styled.p`
+  margin: 0 auto;
+  font-weight: bold;
+`;
+export const UserTag = styled.p`
+  margin: 0 auto;
+`;
+export const UserLocation = styled.p`
+  margin: 0 auto;
+`;
+export const StatsList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 0;
+`;
 
- export const StatsItems=styled.li`
-    display: flex;
-    align-items:center;
-    justify-content:center;
-    flex-wrap:wrap;
-    outline: 1px solid grey;
-    padding: 0 20px;
-    width: 33.33%;
-    
+export const StatsItems = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  outline: 1px solid grey;
+  padding: 0 20px;
+  width: 33.33%;
+`;
 
- `
-
- export const StatsLabel=styled.span`
-    margin-bottom: 10px;
-  font-size:24px;
-
-    
- `
+export const StatsLabel = styled.span`
+  margin-bottom: 10px;
+  font-size: 24px;
+`;
 export const StatsQuantity = styled.span`
-font-size: 30px;
-    font-weight: bold;
-`
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
