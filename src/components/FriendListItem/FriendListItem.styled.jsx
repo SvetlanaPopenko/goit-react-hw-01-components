868afc79a338d401242b7e12911c0b540e862f11
SvetlanaPopenko <spopenko02@gmail.com>
2023-01-17 22:03:25 +0200

    Update FriendListItem.styled.jsx

--- src/components/FriendListItem/FriendListItem.styled.jsx
+++ src/components/FriendListItem/FriendListItem.styled.jsx
@@ -10,36 +10,35 @@ export const FriendCard = styled.li`
   background-color: #26d2f3;
   box-shadow: inset 0px 1px 45px 1px #0571d5;
   width: 250px;
-  :hover{
+  :hover {
     cursor: pointer;
-    
   }
 `;
 
 export const FriendStatus = styled.span`
   display: inline-block;
-  padding:5px;
+  padding: 5px;
   border-radius: 50%;
   width: 15px;
   height: 15px;
-  background-color:white;
+  background-color: white;
   background-color: ${showStatus};
 `;
 
 function showStatus(props) {
-    return props.isOnline ? "green" : "red";
+  return props.isOnline ? 'green' : 'red';
 }
 
-export const FriendAvatar=styled.img`
-    border-radius:25%;
-margin-right: 10px;
-    padding: 10px;
-    box-shadow: 0px 0px 10px 2px rgba(117, 3, 248, 0.81);
-`
+export const FriendAvatar = styled.img`
+  border-radius: 25%;
+  margin-right: 10px;
+  padding: 10px;
+  box-shadow: 0px 0px 10px 2px rgba(117, 3, 248, 0.81);
+`;
 
-export const FriendName=styled.p`
-    color:#4d078e;
-    :hover{
-      color: #FFFFFF;
-    }
-`
+export const FriendName = styled.p`
+  color: #4d078e;
+  :hover {
+    color: #ffffff;
+  }
+`;
